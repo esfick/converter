b93528a3bba79adacf44d397c62b4faebfd92db3
Emma Fick <efick221@gmail.com>
2021-07-27 11:30:13 -0400

    Initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Converter from './Converter';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+              <Converter/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Converter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Converter.css';
+
+
+export default class Converter extends Component {
+    render(){
+        return(
+            <div >
+                yeet
+            </div>
+        );
+    }
+}
